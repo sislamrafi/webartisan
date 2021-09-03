ae2b02dc877f0a0a58196c7db301aeb7c5fa0267
sislamrafi <sislamrafi333@gmail.com>
2021-09-03 21:32:34 +0600

    first commit

---
+++ src/WebArtisanServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Sislamrafi\Webartisan;
+
+use Illuminate\Support\ServiceProvider;
+
+class WebArtisanServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {   
+        $this->loadRoutesFrom('__DIR__'.'/routs/web.php');
+    }
+}
