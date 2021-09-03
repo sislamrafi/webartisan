5a893a6671d8adf5f0cb1840f21d963a39de5fc6
sislamrafi <sislamrafi333@gmail.com>
2021-09-03 22:38:58 +0600

    version 1.0.0

--- src/WebArtisanServiceProvider.php
+++ src/WebArtisanServiceProvider.php
@@ -13,6 +13,6 @@ class WebArtisanServiceProvider extends ServiceProvider
      */
     public function boot()
     {   
-        $this->loadRoutesFrom('__DIR__'.'/routs/web.php');
+        $this->loadRoutesFrom(__DIR__.'/routes/web.php');
     }
 }
---
+++ src/routes/web.php
@@ -0,0 +1,5 @@
+<?php
+
+Route::get('artisan', function () {
+    return "web artisan";
+});
